f3c7a09de4a3901246a040375fc3ce7b0a551450
Qwe1rty <caleb.rhms@gmail.com>
2017-04-08 16:17:31 -0400

    Basic frame scaling and setup

--- src/SnakeFrame.java
+++ src/SnakeFrame.java
@@ -1,13 +1,26 @@
+import java.awt.Dimension;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class SnakeFrame extends JFrame {
-	
+public class SnakeFrame extends JFrame implements WindowListener {
+
 	private SnakePanel panel;
+
+	// 12 wide, 17 high, multiples of 60/85
+	final static double MIN_WIDTH = 60.000;
+	final static double MIN_HEIGHT = 85.000;
+	final static double ASPECT_RATIO = MIN_WIDTH / MIN_HEIGHT;
 	
-	final static double ASPECT_RATIO = 60/85; // 12 wide, 17 high, multiples of 60/85
-	final static int INITIAL_WIDTH = 600;
-	final static int INITIAL_HEIGHT = 850;
+	final static int HORIZONTAL_OFFSET = 18; 
+	final static int VERTICAL_OFFSET = 48;
+	
+	// Default scaling of 10
+	final static int INITIAL_WIDTH = 660;
+	final static int INITIAL_HEIGHT = 935;
 	final static int MAX_FPS = 30;
 
 	public static void main(String[] args) throws Exception {
@@ -15,34 +28,75 @@ public class SnakeFrame extends JFrame {
 		SnakeFrame frame = new SnakeFrame("Snek");
 		frame.setVisible(true);
 	}
-	
+
 	public SnakeFrame(String title) {
-		
+
 		// Frame initialization
 		super(title);
 		setResizable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocation(30,30);
 		setSize(INITIAL_WIDTH, INITIAL_HEIGHT);
-		
+
 		// Create panel with default initializations
-		panel = new SnakePanel(INITIAL_WIDTH, INITIAL_HEIGHT, ASPECT_RATIO, MAX_FPS);
-		
+		panel = new SnakePanel(MIN_WIDTH, MIN_HEIGHT);
+		panel.setBounds(0, 0, INITIAL_WIDTH, INITIAL_HEIGHT);
+		add(panel);
+
 		// Main game loop
-		new Thread(new Runnable() {
+		Thread runGame = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				boolean isRunning = true;
-				while (isRunning) {
-					 try {
-						 panel.updateGame();
-						 panel.render();
-						 
-					 } catch (Exception e) {}
+				
+				long frameCount = 0;
+				while (true) {
+					try {
+						
+						// Get screen dimensions
+						Dimension screenSize = getSize();
+						int width = ((int) screenSize.getWidth()) - HORIZONTAL_OFFSET;
+						int height = ((int) screenSize.getHeight()) - VERTICAL_OFFSET;
+						
+						panel.setBounds(0, 0, width, height);
+						panel.updateGame(width, height, frameCount);
+						panel.render();
+
+						// Placeholder timer
+						try {Thread.sleep(1000 / MAX_FPS);} catch (Exception e) {}
+						
+						// Increment frame counter
+						if (frameCount == Long.MAX_VALUE) frameCount = 0;
+						else frameCount++;
+						
+					} catch (Exception e) {}
 				}
 			}
-		}).start();
-		
+		});
+		runGame.start();
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+	}
+	@Override
+	public void windowClosed(WindowEvent e) {
+		System.exit(0);
+	}
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+	}
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+	}
+	@Override
+	public void windowIconified(WindowEvent e) {
+	}
+	@Override
+	public void windowOpened(WindowEvent e) {
 	}
 
 }
--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -1,35 +1,105 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
-public class SnakePanel extends JPanel implements ActionListener {
+public class SnakePanel extends JPanel {
 	
 	private static final int PLAYER_ONE_UP = KeyEvent.VK_W;
 	private static final int PLAYER_ONE_DOWN = KeyEvent.VK_S;
 	private static final int PLAYER_ONE_LEFT = KeyEvent.VK_A;
 	private static final int PLAYER_ONE_RIGHT = KeyEvent.VK_D;
 	
-	public SnakePanel(int panelWidth, int panelHeight, double aspectRatio, int maxFPS) {
+	private final double minWidth;
+	private final double minHeight;
+	private final double aspectRatio;
+	
+	private int scalingSize = 1;
+	private int panelWidth = 0;
+	private int panelHeight = 0;
+	private int gameWidth = 0;
+	private int gameHeight = 0;
+	
+	// T = top, B = bottom, L = left, R = right 
+	private int TLx;
+	private int TLy;
+	private int TRx;
+	private int TRy;
+	private int BLx;
+	private int BLy;
+	private int BRx;
+	private int BRy;
+	
+	// CONSTRUCTOR
+	public SnakePanel(double minWidth, double minHeight) {
 		setBackground(Color.WHITE);
-		setPreferredSize(new Dimension(panelWidth, panelHeight));
-		
+		this.minWidth = minWidth;
+		this.minHeight = minHeight;
+		this.aspectRatio = this.minWidth / this.minHeight;
 	}
 	
-	public void updateGame() {
+	public void updateGame(int panelWidth, int panelHeight, long frameCount) {
+		
+		// For rendering
+		this.panelWidth = panelWidth;
+		this.panelHeight = panelHeight;
+		
+		// Calculates the scaling size
+		if (aspectRatio * panelHeight < panelWidth) // Wider than is necessary, limited by height
+			scalingSize = (int) (Math.floor(panelHeight / minHeight));
+		else scalingSize = (int) (Math.floor(panelWidth / minWidth)); // Taller than necessary, limited by width
+		
+		// Determine boundaries of actual game
+		gameWidth = (int) (minWidth * scalingSize);
+		gameHeight = (int) (minHeight * scalingSize);
+		TLx = ((panelWidth - gameWidth) / 2);
+		TLy = ((panelHeight - gameHeight) / 2);
+		TRx = TLx + gameWidth;
+		TRy = TLy;
+		BLx = TLx;
+		BLy = TLy + gameHeight;
+		BRx = TRx;
+		BRy = BLy;
 		
 	}
 
 	public void render() {
-		
+		// Forcibly updates the game panel
+		repaint();
+		revalidate();
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent ae) {
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		
+		// Draw borders of the game
+		g.setColor(Color.BLACK);
+//		g.drawRect(TLx - 2, TLy - 2, TRx + 2, TRy);
+//		g.drawRect(TLx - 2, TLy - 2, BLx, BLy + 2);
+//		g.drawRect(BLx - 2, BLy + 2, BRx + 2, BRy);
+//		g.drawRect(BRx + 2, BRy + 2, TRx, TRy - 2);
+//		
+//		g.setColor(Color.PINK);
+		g.drawLine(TLx, TLy, TRx, TRy);
+		g.drawLine(TLx, TLy, BLx, BLy);
+		g.drawLine(BLx, BLy, BRx, BRy);
+		g.drawLine(TRx, TRy, BRx, BRy);
+		
+		// DEBUG INFO
+		g.drawString("Panel width: " + String.valueOf(panelWidth), 10, 18);
+		g.drawString("Panel height: " + String.valueOf(panelHeight), 10, 38);
+		g.drawString("Game width: " + String.valueOf(gameWidth), 10, 58);
+		g.drawString("Game height: " + String.valueOf(gameHeight), 10, 78);
+		g.drawString("Scaling size: " + String.valueOf(scalingSize), 10, 98);
+		g.drawString("TL: (" + String.valueOf(TLx) + ", " + String.valueOf(TLy) + ")", 10, 118);
+		g.drawString("BL: (" + String.valueOf(BLx) + ", " + String.valueOf(BLy) + ")", 10, 138);
 	}
-
+	
 }
